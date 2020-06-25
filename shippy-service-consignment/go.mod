6f2edd2d012801e66d257bf81c1f50f69854163b
Aji Pramono <jipram017@gmail.com>
2020-06-25 18:25:50 +0700

    modified main

--- shippy-service-consignment/go.mod
+++ shippy-service-consignment/go.mod
@@ -7,12 +7,12 @@ go 1.13
 require (
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/golang/protobuf v1.4.2
+	github.com/jipram017/go-ship/shippy-service-user v0.0.0-20200625075110-bfd42e066dd3
 	github.com/jipram017/go-ship/shippy-service-vessel v0.0.0-20200622110530-d257b244e074
+	github.com/micro/go-micro v1.18.0
 	github.com/micro/go-micro/v2 v2.9.0
 	github.com/pkg/errors v0.9.1
 	go.mongodb.org/mongo-driver v1.3.4
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/sys v0.0.0-20200622214017-ed371f2e16b4 // indirect
 	google.golang.org/genproto v0.0.0-20200623002339-fbb79eadd5eb // indirect
 	google.golang.org/grpc v1.30.0 // indirect
 )
--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -7,20 +7,63 @@ import (
 	"os"
 
 	pb "github.com/jipram017/go-ship/shippy-service-consignment/proto/consignment"
+	userService "github.com/jipram017/go-ship/shippy-service-user/proto/user"
 	vesselProto "github.com/jipram017/go-ship/shippy-service-vessel/proto/vessel"
+
+	"github.com/micro/go-micro/metadata"
 	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/client"
+	"github.com/micro/go-micro/v2/server"
+
+	"github.com/pkg/errors"
 )
 
 const (
 	defaultHost = "mongodb://127.0.0.1:27017"
 )
 
+// AuthWrapper is a high-order function which takes a HandlerFunc
+// and returns a function, which takes a context, request and response interface.
+// The token is extracted from the context set in our consignment-cli, that
+// token is then sent over to the user service to be validated.
+// If valid, the call is passed along to the handler. If not,
+// an error is returned.
+
+func authWrapper(fn server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, resp interface{}) error {
+		meta, ok := metadata.FromContext(ctx)
+		if !ok {
+			log.Println(meta)
+			log.Println(ok)
+			return errors.New("No auth metadata found in request")
+		}
+
+		// Note this is now uppercase (not entirely sure why this is...)
+		token := meta["Token"]
+		log.Println("Authenticating with token: ", token)
+
+		// Authenticate here
+		authClient := userService.NewUserService("shippy.service.user", client.DefaultClient)
+		_, err := authClient.ValidateToken(context.Background(), &userService.Token{
+			Token: token,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		err = fn(ctx, req, resp)
+		return err
+
+	}
+}
+
 func main() {
 
 	service := micro.NewService(
 		micro.Name("shippy.service.consignment"),
+		micro.WrapHandler(authWrapper),
 		micro.Version("latest"),
-		micro.WrapHandler(AuthWrapper),
 	)
 
 	// Initialize service
@@ -54,10 +97,3 @@ func main() {
 		log.Panic(err)
 	}
 }
-
-// AuthWrapper is a high-order function which takes a HandlerFunc
-// and returns a function, which takes a context, request and response interface.
-// The token is extracted from the context set in our consignment-cli, that
-// token is then sent over to the user service to be validated.
-// If valid, the call is passed along to the handler. If not,
-// an error is returned.
