bcfda83b220235d114b2627ef6e56b9fe4085d99
Aji Pramono <jipram017@gmail.com>
2020-06-26 10:47:27 +0700

    fix bugs

--- shippy-service-consignment/go.mod
+++ shippy-service-consignment/go.mod
@@ -5,13 +5,14 @@ replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
 go 1.13
 
 require (
-	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/francoispqt/gojay v1.2.13
 	github.com/golang/protobuf v1.4.2
 	github.com/jipram017/go-ship/shippy-service-user v0.0.0-20200625183214-ee0485bcb348
 	github.com/jipram017/go-ship/shippy-service-vessel v0.0.0-20200625181915-954b432d8aef
 	github.com/klauspost/compress v1.10.10 // indirect
 	github.com/micro/go-micro v1.18.0
-	github.com/micro/go-micro/v2 v2.9.0
+	github.com/micro/go-micro/v2 v2.9.1-0.20200625102543-5ab475636ad8
+	github.com/micro/micro/v2 v2.9.2-0.20200625165022-867f44aafdf3 // indirect
 	github.com/pkg/errors v0.9.1
 	go.mongodb.org/mongo-driver v1.3.4
 	golang.org/x/net v0.0.0-20200625001655-4c5254603344
--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -14,8 +14,8 @@ import (
 
 	"github.com/micro/go-micro/metadata"
 	"github.com/micro/go-micro/v2"
-	"github.com/micro/go-micro/v2/client"
-	"github.com/micro/go-micro/v2/server"
+	cl "github.com/micro/go-micro/v2/client"
+	servo "github.com/micro/go-micro/v2/server"
 )
 
 const (
@@ -28,8 +28,8 @@ const (
 // token is then sent over to the user service to be validated.
 // If valid, the call is passed along to the handler. If not,
 // an error is returned.
-func AuthWrapper(fn server.HandlerFunc) server.HandlerFunc {
-	return func(ctx context.Context, req server.Request, resp interface{}) error {
+func AuthWrapper(fn servo.HandlerFunc) servo.HandlerFunc {
+	return func(ctx context.Context, req servo.Request, resp interface{}) error {
 		meta, ok := metadata.FromContext(ctx)
 		if !ok {
 			return errors.New("no auth meta-data found in request")
@@ -40,7 +40,7 @@ func AuthWrapper(fn server.HandlerFunc) server.HandlerFunc {
 		log.Println("Authenticating with token: ", token)
 
 		// Auth here
-		authClient := userService.NewUserService("go.micro.srv.user", client.DefaultClient)
+		authClient := userService.NewUserService("go.micro.srv.user", cl.DefaultClient)
 		_, err := authClient.ValidateToken(context.Background(), &userService.Token{
 			Token: token,
 		})
@@ -60,7 +60,6 @@ func main() {
 		micro.WrapHandler(AuthWrapper),
 	)
 
-	log.Println("masuk6")
 	// Initialize service
 	service.Init()
 
@@ -75,22 +74,18 @@ func main() {
 	}
 	defer client.Disconnect(context.Background())
 
-	log.Println("masuk7")
 	consignmentCollection := client.Database("shippy").Collection("consignments")
 	repository := &MongoRepository{consignmentCollection}
 	vesselClient := vesselProto.NewVesselService("go.micro.srv.vessel", service.Client())
-	log.Println("masuk8")
 
 	h := &handler{repository, vesselClient}
 
 	// Register our implementation with
 	if err := pb.RegisterShippingServiceHandler(service.Server(), h); err != nil {
-		log.Println("masuk9")
 		log.Panic(err)
 	}
 
 	if err := service.Run(); err != nil {
-		log.Println("masuk10")
 		log.Panic(err)
 	}
 }
